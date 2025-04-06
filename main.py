70bda72455963d24c9d2c6e2865bee147d96f061
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-07 00:10:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,22 @@ import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
+
+
+import threading
+import requests
+import time
+ 
+def keep_alive():
+    while True:
+        try:
+            requests.get("https://famous-addi-moxibeatz21-7ed6f21b.koyeb.app/")
+        except:
+            pass
+        time.sleep(90)  # Ping every 5 minutes
+ 
+threading.Thread(target=keep_alive, daemon=True).start()
+
+
 if __name__ == "__main__":
     Bot().run()
