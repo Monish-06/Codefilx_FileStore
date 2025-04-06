d8b415fd6a9875a9748c55154bf52a1442f275e9
Monish-06 <70092044+Monish-06@users.noreply.github.com>
2025-04-07 00:30:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def keep_alive():
             requests.get("https://famous-addi-moxibeatz21-7ed6f21b.koyeb.app/")
         except:
             pass
-        time.sleep(90)  # Ping every 5 minutes
+        time.sleep(10)  # Ping every 5 minutes
  
 threading.Thread(target=keep_alive, daemon=True).start()
 
